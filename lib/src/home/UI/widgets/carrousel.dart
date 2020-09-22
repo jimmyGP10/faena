b0ef0413c1b908e679e50bbe7294100c13355c17
jimmyGP10 <jimmyfuentesGP@gmail.com>
2020-09-22 16:49:27 -0600

    commit J

--- lib/src/home/UI/widgets/carrousel.dart
+++ lib/src/home/UI/widgets/carrousel.dart
@@ -12,69 +12,91 @@ class Carrousel extends StatefulWidget {
 }
 
 class _CarrouselState extends State<Carrousel> {
-  List imgList = [
-    'https://image.freepik.com/foto-gratis/familia-feliz-ninos-desempacando-cajas-que-mudan-su-nuevo-hogar_1163-4830.jpg',
-    'https://image.freepik.com/foto-gratis/familia-feliz-ninos-desempacando-cajas-que-mudan-su-nuevo-hogar_1163-4830.jpg',
-    'https://image.freepik.com/foto-gratis/familia-feliz-ninos-desempacando-cajas-que-mudan-su-nuevo-hogar_1163-4830.jpg',
-  ];
+  int _currentIndex = 0;
+  onPageChange(int index, CarouselPageChangedReason changedReason) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CarouselSlider(
-        options: CarouselOptions(
-          height: 200.0,
-          autoPlay: true,
-          enableInfiniteScroll: true,
-          autoPlayInterval: Duration(seconds: 3),
-          autoPlayAnimationDuration: Duration(milliseconds: 1000),
-          pauseAutoPlayOnTouch: true,
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.horizontal,
-        ),
-        items: widget.categoryList.map((i) {
-          return Builder(builder: (BuildContext context) {
-            return GestureDetector(
-                child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        image: DecorationImage(
-                            image: NetworkImage(i.photoURL), fit: BoxFit.cover),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Container(
-                              padding: EdgeInsets.all(10),
-                              width: MediaQuery.of(context).size.width,
-                              height: 70,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(15),
-                                      bottomRight: Radius.circular(15)),
-                                  color: Color.fromRGBO(255, 255, 255, 0.92)),
-                              child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(i.name,
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600)),
-                                    // Expanded(child: Text(i.description))
-                                  ]))
-                        ])),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Services(uid: i.uid)));
-                });
-          });
-        }).toList(),
-      ),
-    );
+    return Column(children: <Widget>[
+      CarouselSlider(
+          options: CarouselOptions(
+              height: 200.0,
+              autoPlay: true,
+              enableInfiniteScroll: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 1000),
+              pauseAutoPlayOnTouch: true,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeCenterPage: true,
+              scrollDirection: Axis.horizontal,
+              onPageChanged: onPageChange),
+          items: widget.categoryList.map((i) {
+            return Builder(builder: (BuildContext context) {
+              return GestureDetector(
+                  child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: NetworkImage(i.photoURL),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.all(10),
+                                width: MediaQuery.of(context).size.width,
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                        bottomLeft: Radius.circular(15),
+                                        bottomRight: Radius.circular(15)),
+                                    color: Color.fromRGBO(255, 255, 255, 0.92)),
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(i.name,
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600)),
+                                      // Expanded(child: Text(i.description))
+                                    ]))
+                          ])),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Services(uid: i.uid)));
+                  });
+            });
+          }).toList()),
+      Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: map<Widget>(widget.categoryList, (index, url) {
+            return Container(
+              width: 10.0,
+              height: 10.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color:
+                      _currentIndex == index ? Colors.blueAccent : Colors.grey),
+            );
+          })),
+    ]);
   }
 }
